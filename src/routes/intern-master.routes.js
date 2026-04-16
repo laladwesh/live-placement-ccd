ffba54bb66d91978167cd80214d594d8db366bf9
laladwesh <guptaavinash302@gmail.com>
2026-04-16 18:05:21 +0530

    Add enhancements : src/routes/intern-master.routes.js

---
+++ src/routes/intern-master.routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { authMiddleware, permit } from "../middleware/auth.middleware.js";
+import { getInternMasterData, updateInternPlacement } from "../controllers/intern-master.controller.js";
+
+const router = express.Router();
+
+router.get("/intern-master", authMiddleware, permit("viewer", "admin"), getInternMasterData);
+router.patch("/intern-master/:id/placement", authMiddleware, permit("viewer", "admin"), updateInternPlacement);
+
+export default router;
